d127e180daea4bcb0c3d35008a0f30e94b5392ca
Shoshi <s0556722535@gmail.com>
2024-04-30 16:20:50 +0300

    finish fix to models

--- jobSearch_server/Models/User.cs
+++ jobSearch_server/Models/User.cs
@@ -6,7 +6,7 @@
         public string userName { get; set; }
         public string password { get; set; }
         public Field jobSearchField { get; set; }
-        public int CVsSentsAmount { get; set; }
+        public int cVsSentsAmount { get; set; }
 
          public User(int id, string userName,string password,Field jobSearchField,int CVsSentsAmount)
     {
@@ -14,7 +14,7 @@
         this.userName=userName;
         this.password=password;
         this.jobSearchField=jobSearchField;
-        this.CVsSentsAmount=CVsSentsAmount;
+        this.cVsSentsAmount=CVsSentsAmount;
     }
     
     }
